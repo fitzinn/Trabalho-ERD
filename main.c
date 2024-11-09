4dc922c55b75235f882c2f1d46c5e72eb8539f47
Fitz <142354039+fitzinn@users.noreply.github.com>
2024-11-09 01:39:21 -0300

    main
    base das funções a serem usadas

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//Selection Sort: Um algoritmo simples que repetidamente seleciona o menor elemento restante.
+void selectionSort(int arr[], int n) {
+
+}
+
+//Insertion Sort: Constrói a lista final um item de cada vez, como organizar cartas de baralho na mão.
+void insertionSort(int arr[], int n) {
+
+}
+
+//Bubble Sort: Compara pares adjacentes e os troca se estiverem fora de ordem.
+void bubbleSort(int arr[], int n) {
+
+}
+
+//Merge Sort: Um algoritmo eficiente que divide a lista, ordena as partes e depois as mescla.
+void mergeSort(int arr[], int n) {
+
+}
+
+
+//Quick Sort: Um algoritmo rápido que usa a estratégia de dividir e conquistar.
+void quickSort(int arr[], int n) {
+
+}
+
+
+//Heap Sort: Utiliza uma estrutura de dados chamada heap para ordenar os elementos.
+void heapSort(int arr[], int n) {
+
+}
+
+
+//main para testes
+int main() {
+
+}
