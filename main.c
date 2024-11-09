ef687c246b63c8fff7b45fb2873e541bee70aaeb
Fitz <142354039+fitzinn@users.noreply.github.com>
2024-11-09 02:53:58 -0300

    bubbleSort
    função bubbleSort funcional

--- main.c
+++ main.c
@@ -33,7 +33,15 @@ void insertionSort(int array[], int n) {
 
 //Bubble Sort: Compara pares adjacentes e os troca se estiverem fora de ordem.
 void bubbleSort(int array[], int n) {
-
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(array[j] > array[j + 1]) {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
 }
 
 //Merge Sort: Um algoritmo eficiente que divide a lista, ordena as partes e depois as mescla.
